6fd54ce60a716462d4f95d3805ab83b68dc7a091
chris48s <c.mshaw@yahoo.co.uk>
2016-09-13 20:28:12 +0100

    update to match new base scraper format

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
-from geojson_scraper import scrape
+from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
 
 districts_url = "http://inspire.sthelens.gov.uk/geoserver/ows?service=WFS&version=1.3.0&request=GetFeature&typeNames=INSPIRE%3Apolling_districts&outputFormat=json"
 council_id = 'E08000013'
 
 
-scrape(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper = GeoJsonScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper.scrape()
